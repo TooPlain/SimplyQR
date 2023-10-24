ac3c55e770927e3d74869e50a4d764e341168561
TooPlain <TooPlain@galbo.io>
2023-10-24 07:08:39 -0400

    Initial Commit

---
+++ SimplyQR/SimplyQRApp.swift
@@ -0,0 +1,17 @@
+//
+//  SimplyQRApp.swift
+//  SimplyQR
+//
+//  Created by parity on 10/24/23.
+//
+
+import SwiftUI
+
+@main
+struct SimplyQRApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
