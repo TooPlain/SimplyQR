ba789bd9d3dd5afe092bd276e6ac74f9562d84e3
TooPlain <TooPlain@galbo.io>
2023-10-30 08:06:14 -0400

    Prototyping/planning on what we should impl boilerplate stuffs

---
+++ SimplyQR/History/ScanHistoryManager.swift
@@ -0,0 +1,8 @@
+//           
+//  ScanHistoryManager.swift          
+//  SimplyQR 
+// 
+//  Created by TooPlain on 10/30/23.
+//
+
+import Foundation
---
+++ SimplyQR/ScanData/ScanDataModel.swift
@@ -0,0 +1,8 @@
+//           
+//  ScanDataModel.swift          
+//  SimplyQR 
+// 
+//  Created by TooPlain on 10/30/23.
+//
+
+import Foundation
