46b9823509aedc07a26f1e4803d8cbe0801770e4
TooPlain <TooPlain@galbo.io>
2023-10-30 07:44:39 -0400

    Start History View

---
+++ SimplyQR/Views/History/ScanHistoryView.swift
@@ -0,0 +1,18 @@
+//           
+//  ScanHistoryView.swift          
+//  SimplyQR 
+// 
+//  Created by TooPlain on 10/30/23.
+//
+
+import SwiftUI
+
+struct ScanHistoryView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ScanHistoryView()
+}
