70ad5bc79ad5fa5bb87beadcec8b2811c7da1dd2
TooPlain <TooPlain@galbo.io>
2023-10-30 10:55:45 -0400

    Added  app version handling (Basic)

--- SimplyQR/SimplyQRApp.swift
+++ SimplyQR/SimplyQRApp.swift
@@ -47,3 +47,16 @@ struct SimplyQRApp: App {
         }
     }
 }
+
+
+extension Bundle {
+    var releaseVersionNumber: String? {
+        return infoDictionary?["CFBundleShortVersionString"] as? String
+    }
+    var buildVersionNumber: String? {
+        return infoDictionary?["CFBundleVersion"] as? String
+    }
+    var releaseVersionNumberPretty: String {
+        return "v\(releaseVersionNumber ?? "1.0.0")"
+    }
+}
