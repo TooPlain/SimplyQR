939ae28594ccf4674f7901c2d6326f7adc0c36c3
TooPlain <TooPlain@galbo.io>
2023-10-30 10:20:27 -0400

    Gallery Scan Done

--- SimplyQR/Views/Scanner/ScannerView.swift
+++ SimplyQR/Views/Scanner/ScannerView.swift
@@ -7,12 +7,15 @@
 
 import SwiftUI
 import CodeScanner
+import PhotosUI
 
 struct ScannerView: View {
     @State private var isPresentingAlert = false
     @State private var scannedCode: String?
     
     @State private var isPresetingPicker = false
+    @State private var selectedItem: PhotosPickerItem? = nil
+    @State private var isTorchOn = false
 
     //Maybe since we have a tab bar call this view directly?
     
@@ -21,15 +24,18 @@ struct ScannerView: View {
         
         // For now a dialog with the scanned information for display and the option to copy to clipboard.
         // Will add like a built in web browser view or handle if the text is a link for an app (eg discord,crypto wallet, etc)
+        // Work on failure handling when scan code is malformed etc
         NavigationStack{
             VStack{
-                CodeScannerView(codeTypes: [.qr,.ean13,.code128,.dataMatrix,.pdf417]) { response in
+                CodeScannerView(codeTypes: [.qr,.ean13,.code128,.dataMatrix,.pdf417],isTorchOn: isTorchOn, isGalleryPresented: $isPresetingPicker) { response in
                     if case let .success(result) = response {
                         scannedCode = result.string
                         isPresentingAlert = true
                     }
                 }.alert(scannedCode ?? "Nul", isPresented: $isPresentingAlert) {
-                    Button("Okay", role: .cancel) {isPresentingAlert = false}
+                    Button("Okay", role: .cancel) {
+                        isPresentingAlert = false
+                    }
                     Button("Copy Clipboard"){
                         UIPasteboard.general.string = scannedCode
                         isPresentingAlert = false
@@ -37,10 +43,21 @@ struct ScannerView: View {
                 }
             }
         }.toolbar {
+            ToolbarItem(placement: .topBarLeading) {
+                Button {
+                    isTorchOn = !isTorchOn
+                } label: {
+                    Image(systemName: "flashlight.on.fill")
+                }
+
+            }
             ToolbarItem(placement: .topBarTrailing) {
-                Button(action: {
-                    
-                }) {
+//                PhotosPicker(selection: $selectedItem, matching: .images, photoLibrary: .shared()) {
+//                    Image(systemName: "photo.on.rectangle.angled")
+//                }
+                Button {
+                    isPresetingPicker = !isPresetingPicker
+                } label: {
                     Image(systemName: "photo.on.rectangle.angled")
                 }
             }
